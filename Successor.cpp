e5e4545ee5d98e200989f77385fc36e25c892424
Anurag Jain <jainanurag7nov@gmail.com>
2024-01-20 23:51:34 +0530

    add feat successor.cpp

---
+++ Successor.cpp
@@ -0,0 +1,10 @@
+// Approach
+
+//  method 1
+
+// inorder nikalke key value se phele vala ya uske baad vaala value return kr do 
+// 
+// method 2
+// predecessor= node se just chota element  yani left subtree me maximum element //
+// successor = right subtree me minimum value 
+
